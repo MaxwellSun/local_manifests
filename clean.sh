4a014f112e4ebcdbeec7a0f19940d5127521bf3f
Ivan Meler <i_ivan@windowslive.com>
2015-02-21 17:25:30 +0100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
-
-rm -rf /hardware
-cd vendor
-rm -rf /samsung
-cd ..
-cd frameworks
-rm -rf native
-rm -rf av
-cd ..
-cd vendor
-rm -rf /samsung
-cd ..
-cd /.repo/local_manifests/
-rm -rf roomservice.xml
-cd ../..
-rm -rf /device
-rm -rf /kernel
-rm -rf /bionic
-cp ~/home/ivanmeler/android/system/out/target/product/*/cm-12-*.zip ~/home/ivanmeler/out/
-source build/envsetup.sh
-make clean
-cd /.repo/local_manifests/
-cd ../..
-repo sync
+rm -rf frameworks
+rm -rf vendor
+rm -rf device
+rm -rf hardware
+rm -rf kernel
+rm -rf bionic
